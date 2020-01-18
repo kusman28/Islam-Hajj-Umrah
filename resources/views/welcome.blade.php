c88368a4d65502e300f5c39cd513cab61c92e5a3
K. Usman <kusman758@gmail.com>
2020-01-19 00:21:24 +0800

    some text :heart:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -282,7 +282,7 @@ About Us Section
 
 <header class="section-header">
 <h3>About Us</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<p>About Us content descrption here.</p>
 </header>
 
 <div class="row about-cols">
@@ -295,7 +295,7 @@ About Us Section
 </div>
 <h2 class="title"><a href="#">Our Mission</a></h2>
 <p>
-Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Our Mission content description here.
 </p>
 </div>
 </div>
@@ -308,7 +308,7 @@ Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et
 </div>
 <h2 class="title"><a href="#">Our Plan</a></h2>
 <p>
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+Our Plan content description here.
 </p>
 </div>
 </div>
@@ -321,7 +321,7 @@ Sed ut perspiciatis unde omnis iste natus error sit voluptatem  doloremque lauda
 </div>
 <h2 class="title"><a href="#">Our Vision</a></h2>
 <p>
-Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+Our Vision content description here.
 </p>
 </div>
 </div>
